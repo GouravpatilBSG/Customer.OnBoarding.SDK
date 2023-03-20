a45678dd021d977346c3eeeb809a6a024b785024
GouravpatilBSG <69052714+GouravpatilBSG@users.noreply.github.com>
2023-03-20 09:53:28 +0530

    activities added

--- app/src/main/java/app/one/androidlibrary/MainActivity.java
+++ app/src/main/java/app/one/androidlibrary/MainActivity.java
@@ -2,16 +2,28 @@ package app.one.androidlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import app.one.toasty.ToastActivty;
 import app.one.toasty.Toaster;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ToastActivty.class));
+            }
+        });
         Toaster.simpleToast(this,"This is my first Library");
     }
 }
--- toasty/src/main/java/app/one/toasty/SecondActivity.java
+++ toasty/src/main/java/app/one/toasty/SecondActivity.java
@@ -2,29 +2,24 @@ package app.one.toasty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class ToastActivty extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_toast_activty);
+        setContentView(R.layout.activity_second);
         btnSubmit = findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), SecondActivity.class));
+                //startActivity(new Intent(getApplicationContext(), ToastActivty.class));
             }
         });
-
     }
-
-
 }
